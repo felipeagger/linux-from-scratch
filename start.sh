671a1e4cbfe188e99126867bec4cad9c313e86ef
Filipe Xavier <43504172+felipeagger@users.noreply.github.com>
2024-09-15 13:02:01 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,7 @@ qemu-system-riscv64 -nographic -machine virt -bios default \
   -kernel KernelImage \
   -initrd initramfs.cpio.gz \
   -m 512M \
-  -append "console=ttyS0 loglevel=7 earlyprintk=serial"
+  -append "console=ttyS0 loglevel=7 earlyprintk=serial" \
+  -net nic -net user \
+  -netdev user,id=mynet0 \
+  -device virtio-net-device,netdev=mynet0
