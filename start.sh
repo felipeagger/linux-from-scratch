0047a5327a19f4e69b344b42c59a28224d1e52c2
Filipe Xavier <43504172+felipeagger@users.noreply.github.com>
2024-09-15 16:04:36 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -3,8 +3,6 @@
 qemu-system-riscv64 -nographic -machine virt -bios default \
   -kernel KernelImage \
   -initrd initramfs.cpio.gz \
-  -m 512M \
+  -m 512M -smp 2 \
   -append "console=ttyS0 loglevel=7 earlyprintk=serial" \
-  -net nic -net user \
-  -netdev user,id=mynet0 \
-  -device virtio-net-device,netdev=mynet0
+  -device virtio-net-device,netdev=eth0 -netdev user,id=eth0
