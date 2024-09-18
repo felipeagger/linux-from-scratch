f916338b14519d4119dc93643bf3d308714bbbb1
Filipe Xavier <43504172+felipeagger@users.noreply.github.com>
2024-09-18 19:10:14 -0300

    Update start.sh

--- x86_64/start.sh
+++ x86_64/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 qemu-system-x86_64 -kernel bzImgKernel6LLVMx8664 \
-  -initrd initramfs.cpio.gz -nographic -m 512M -smp 2 \
-  -append "console=ttyS0 loglevel=7 earlyprintk=serial" \
+  -initrd initramfs.cpio.gz -nographic -m 1024M -smp 2 \
+  -append "console=ttyS0 loglevel=7 earlyprintk=serial ip=dhcp" \
   -device virtio-net-pci,netdev=eth0 -netdev user,id=eth0
