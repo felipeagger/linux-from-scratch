58940b1199eeb28a615fd775bd3a4cbbb10b396a
Filipe Xavier <43504172+felipeagger@users.noreply.github.com>
2024-09-21 13:25:18 -0300

    Update start.sh

--- riscv64/start.sh
+++ riscv64/start.sh
@@ -4,5 +4,5 @@ qemu-system-riscv64 -nographic -machine virt -bios default \
   -kernel KernelImage \
   -initrd initramfs.cpio.gz \
   -m 512M -smp 2 \
-  -append "console=ttyS0 loglevel=7 earlyprintk=serial" \
+  -append "console=ttyS0 loglevel=7 earlyprintk=serial ip=dhcp" \
   -device virtio-net-device,netdev=eth0 -netdev user,id=eth0
