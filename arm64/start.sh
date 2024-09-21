17f48a6329c7baa324a9d1ea1ee5eb49fae11dab
Filipe Xavier <43504172+felipeagger@users.noreply.github.com>
2024-09-21 14:31:02 -0300

    Update start.sh

--- arm64/start.sh
+++ arm64/start.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-qemu-system-aarch64 -nographic -machine virt \
+qemu-system-aarch64 -nographic -M virt \
   -kernel kernelRustLLVMImg.gz \
   -initrd initramfs.cpio.gz \
   -cpu cortex-a57 -m 512M \
-  -append "console=ttyAMA0 loglevel=7 earlyprintk=serial ip=dhcp" \
-  -net nic -net user \
-  -netdev user,id=mynet0 \
-  -device virtio-net-device,netdev=mynet0
+  -netdev user,id=n0 -device virtio-net-device,netdev=n0 \
+  -append "console=ttyAMA0 ip=dhcp loglevel=7 earlyprintk=serial"
