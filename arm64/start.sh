2da8773c737d4af84532921a86089edb577137d0
Filipe Xavier <43504172+felipeagger@users.noreply.github.com>
2024-09-21 14:05:54 -0300

    Add files via upload

---
+++ arm64/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+qemu-system-aarch64 -nographic -machine virt \
+  -kernel kernelRustLLVMImg \
+  -initrd initramfs.cpio.gz \
+  -cpu cortex-a57 -m 1024M \
+  -append "console=ttyAMA0 init=/sbin/init loglevel=7 earlyprintk=serial ip=dhcp" \
+  -net nic -net user \
+  -netdev user,id=mynet0 \
+  -device virtio-net-device,netdev=mynet0
