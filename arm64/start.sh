6f7c18818686ed189bc1ac3f4832b5c5472133db
Filipe Xavier <43504172+felipeagger@users.noreply.github.com>
2024-09-21 14:07:59 -0300

    Update start.sh

--- arm64/start.sh
+++ arm64/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 qemu-system-aarch64 -nographic -machine virt \
-  -kernel kernelRustLLVMImg \
+  -kernel kernelRustLLVMImg.gz \
   -initrd initramfs.cpio.gz \
   -cpu cortex-a57 -m 1024M \
   -append "console=ttyAMA0 init=/sbin/init loglevel=7 earlyprintk=serial ip=dhcp" \
